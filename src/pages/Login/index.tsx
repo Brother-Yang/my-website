17d02058758c5468848e3fae2f64e3a306df88ca
Brother-Yang <450028105@qq.com>
2023-05-29 16:30:42 +0800

    feat(Login): 更换ui

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,42 +1,24 @@
 import React from 'react'
 
-import { Input } from 'antd'
-
-import SystemFormItemGridLayout, { Label, Value } from '@/components/SystemFormItemGridLayout'
+import { Button, Input } from 'antd'
 
 import styles from './index.less'
 
 const Login = () => {
   return (
-    <div className={styles.Wraps}>
-      <div className={styles.LoginBox}>
-        <SystemFormItemGridLayout
-          data={{
-            width: '100%',
-            columnCount: 1,
-            colgroup: ['120px', '1fr'],
-            children: [
-              {
-                key: 'account',
-                label: <Label>账号</Label>,
-                value: (
-                  <Value>
-                    <Input />
-                  </Value>
-                ),
-              },
-              {
-                key: 'password',
-                label: <Label>密码</Label>,
-                value: (
-                  <Value>
-                    <Input />
-                  </Value>
-                ),
-              },
-            ],
-          }}
-        />
+    <div className={styles.Container}>
+      <div className={styles.LoginWrapper}>
+        <div className={styles.Header}>Login</div>
+        <div className={styles.FormWrapper}>
+          <Input placeholder="username" size="large" className={styles.InputItem} />
+          <Input.Password placeholder="password" className={styles.InputItem} size="large" />
+          <Button className={styles.Btn} size="large">
+            sign in
+          </Button>
+          <Button className={styles.Btn} size="large">
+            sign up
+          </Button>
+        </div>
       </div>
     </div>
   )
