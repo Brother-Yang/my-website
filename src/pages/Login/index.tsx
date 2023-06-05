414379fbfcd301127f50ca1073795f7f12b18b46
Brother-Yang <450028105@qq.com>
2023-06-05 15:54:03 +0800

    feat(Login): 静态登陆

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,23 +1,51 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
-import { Button, Input } from 'antd';
+import { Button, Input, Form } from 'antd';
+import _ from 'lodash';
 
 import styles from './index.less';
 
 const Login = () => {
+  const [form] = Form.useForm();
+
+  const navigate = useNavigate();
+
+  const user = {
+    username: 'admin',
+    password: '123',
+  };
+
+  const onFinish = (values: typeof user) => {
+    if (_.isEqual(values, user)) {
+      navigate('/reduxToolkitDemo');
+    }
+  };
+
   return (
     <div className={styles.Container}>
       <div className={styles.LoginWrapper}>
         <div className={styles.Header}>Login</div>
         <div className={styles.FormWrapper}>
-          <Input placeholder="username" size="large" className={styles.InputItem} />
-          <Input.Password placeholder="password" className={styles.InputItem} size="large" />
-          <Button className={styles.Btn} size="large">
-            sign in
-          </Button>
-          <Button className={styles.Btn} size="large">
-            sign up
-          </Button>
+          <Form form={form} onFinish={onFinish} initialValues={user}>
+            <Form.Item name={'username'}>
+              <Input placeholder="username" size="large" className={styles.InputItem} />
+            </Form.Item>
+
+            <Form.Item name={'password'}>
+              <Input.Password placeholder="password" className={styles.InputItem} size="large" />
+            </Form.Item>
+
+            <Form.Item>
+              <Button className={styles.Btn} size="large" htmlType="submit">
+                sign in
+              </Button>
+            </Form.Item>
+
+            <Button className={styles.Btn} size="large">
+              sign up
+            </Button>
+          </Form>
         </div>
       </div>
     </div>
