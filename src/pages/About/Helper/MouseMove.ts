7e7d774f18ba0ac73d8afff2fee952c300cd622f
Hongbin <2218176087@qq.com>
2023-06-16 20:43:52 +0800

    准备第二屏

--- src/pages/About/Helper/MouseMove.ts
+++ src/pages/About/Helper/MouseMove.ts
@@ -2,7 +2,7 @@
  * @Author: hongbin
  * @Date: 2023-06-15 12:00:21
  * @LastEditors: hongbin
- * @LastEditTime: 2023-06-15 17:25:32
+ * @LastEditTime: 2023-06-16 20:38:44
  * @Description: 鼠标移动 镜头晃动
  */
 
@@ -20,7 +20,7 @@ export class MouseMove {
     //@ts-ignore
     this.point.add({ x: e.movementX, y: e.movementY, z: 0 });
     this.point.normalize().divideScalar(100);
-    ThreeHelper.instance.controls.target.set(0, 0, 0);
+    // ThreeHelper.instance.controls.target.set(0, 0, 0);
     ThreeHelper.instance.camera.position.add(this.point);
   }
 
