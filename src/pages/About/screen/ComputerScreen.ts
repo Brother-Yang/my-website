2380376091c13fa6365970c29af6ddf2d136dfee
Hongbin <2218176087@qq.com>
2023-06-25 22:09:39 +0800

    加入人物模型 但需解决第一次加载的卡顿问题

--- src/pages/About/screen/ComputerScreen.ts
+++ src/pages/About/screen/ComputerScreen.ts
@@ -2,31 +2,49 @@
  * @Author: hongbin
  * @Date: 2023-06-16 20:46:18
  * @LastEditors: hongbin
- * @LastEditTime: 2023-06-24 21:59:12
+ * @LastEditTime: 2023-06-25 21:58:52
  * @Description:第二屏幕 "电脑"
  */
 
-import { Box3Helper, Group } from 'three';
+import { AnimationMixer, Group, MeshStandardMaterial } from 'three';
 import { ThreeHelper } from '../ThreeHelper';
 import { gsap } from 'gsap';
-import { MyEnvironment } from './MyEnv';
 import { getBoxSize, getWindowSize } from '../ThreeHelper/utils';
+import { RoomEnvironment } from 'three/examples/jsm/environments/RoomEnvironment';
 
 export class ComputerScreen {
   group = new Group();
   onEnter = () => {};
+  boxSize: any;
+  envMap: THREE.Texture;
+  animation = (p: number) => {};
+  peopleModel = new Group();
 
   constructor() {
+    const env = new RoomEnvironment();
+    this.envMap = ThreeHelper.instance.pmremGenerator.fromScene(env, 0.01).texture;
+    this.group.add(this.peopleModel);
+
     this.loadModel();
     ThreeHelper.instance.add(this.group);
+  }
+
+  setEnv(group: Object3D) {
+    group.traverse((obj: StandardMesh) => {
+      if (obj.isMesh) {
+        obj.material.envMap = this.envMap;
+      }
+    });
   }
 
   /**
    * 将模型设置到窗口上方
    */
   setModelOnWindowAbove(model: Object3D) {
-    const windowHeight = getWindowSize(ThreeHelper.instance.camera).height / 2;
+    const windowSize = getWindowSize(ThreeHelper.instance.camera);
+    const windowHeight = windowSize.height / 2;
     const boxSize = getBoxSize(model);
+    this.boxSize = boxSize;
     // 除却y轴 如果z轴很宽 那么后面也会漏出来 所以加上z轴长度
     const modelHeight = boxSize.y / 2 + boxSize.z / 2;
     return modelHeight + windowHeight;
@@ -37,19 +55,12 @@ export class ComputerScreen {
       console.log(gltf);
       this.group.add(gltf.scene);
       this.enterAnimation();
-
+      this.loadPeopleModel();
       const y = this.setModelOnWindowAbove(gltf.scene);
       this.group.position.y = y;
       this.group.userData.startPositionY = y;
 
-      const env = MyEnvironment();
-      const envMap = ThreeHelper.instance.pmremGenerator.fromScene(env, 0.01).texture;
-
-      gltf.scene.traverse((obj: StandardMesh) => {
-        if (obj.isMesh) {
-          obj.material.envMap = envMap;
-        }
-      });
+      this.setEnv(gltf.scene);
 
       const 底座 = gltf.scene.getObjectByName('底座') as StandardMesh;
       const apple_logo = gltf.scene.getObjectByName('apple_logo') as StandardMesh;
@@ -68,8 +79,9 @@ export class ComputerScreen {
 
   enterAnimation() {
     this.group.position.z = 6;
-    const loadContainer = this.group.getObjectByName('loadContainer') as Object3D;
-
+    /**
+     * 电脑出现
+     */
     gsap.timeline({
       scrollTrigger: {
         trigger: '#container',
@@ -89,10 +101,11 @@ export class ComputerScreen {
       },
     });
 
-    const screen = this.group.getObjectByName('屏幕') as StandardMesh;
-    screen.material.color.setScalar(0);
-    screen.material.roughness = 0;
+    const loadContainer = this.group.getObjectByName('loadContainer') as Object3D;
 
+    /**
+     * logo出现
+     */
     gsap.timeline({
       scrollTrigger: {
         trigger: '#container',
@@ -100,21 +113,15 @@ export class ComputerScreen {
         end: innerHeight * 2.5,
         onUpdate: (event) => {
           const p = event.progress;
-          // this.group.position.z = 6 * p;
-          // screen.material.color.setScalar(event.progress * 0.1);
-          screen.material.metalness = event.progress;
-          // screen.material.roughness = 1 - event.progress;
           loadContainer.position.z = p * 0.04;
         },
-        onLeave: (e) => {
-          console.log('leave');
-        },
-        onEnterBack: () => {
-          console.log('onEnterBack');
-        },
+        onLeave: (e) => {},
+        onEnterBack: () => {},
       },
     });
-
+    /**
+     * 进度条推进
+     */
     const progressBar = loadContainer.getObjectByName('progressBar') as StandardMesh;
 
     gsap.timeline({
@@ -134,27 +141,81 @@ export class ComputerScreen {
       },
     });
 
-    const apple_logo = loadContainer.getObjectByName('apple_logo') as StandardMesh;
+    /**
+     * 屏幕亮起 logo退却 人物探身
+     */
+    const screen = this.group.getObjectByName('屏幕') as StandardMesh;
+
+    screen.material.color.setScalar(0);
+
+    const t4 = gsap.timeline({
+      scrollTrigger: {
+        trigger: '#container',
+        start: innerHeight * 3.5,
+        end: innerHeight * 4.0,
+        onUpdate: (event) => {
+          loadContainer.position.z = 0.04 - event.progress / 15;
+          screen.material.color.setScalar(event.progress);
+          this.peopleModel.position.z = -0.4 + 0.2 * event.progress;
+        },
+        onLeave: (e) => {},
+        onEnterBack: () => {},
+      },
+    });
 
     gsap.timeline({
       scrollTrigger: {
         trigger: '#container',
-        start: innerHeight * 3.5,
-        end: innerHeight * 4.5,
+        start: innerHeight * 4,
+        end: innerHeight * 5,
         onUpdate: (event) => {
-          this.group.rotation.y = event.progress * -0.7;
-
-          this.group.position.y = event.progress * -1;
-          this.group.position.z = event.progress * -2 + 6;
-          this.group.position.x = event.progress * -4;
+          this.peopleModel.position.z = -0.2 + 0.2 * event.progress;
+          this.animation(event.progress);
         },
-        onLeave: (e) => {
-          console.log('leave');
-        },
-        onEnterBack: () => {
-          console.log('onEnterBack');
-        },
+        onLeave: (e) => {},
+        onEnterBack: () => {},
       },
+    });
+
+    // const xLength = getWindowSize(ThreeHelper.instance.camera).width / 2;
+
+    // gsap.timeline({
+    //   scrollTrigger: {
+    //     trigger: '#container',
+    //     start: innerHeight * 3.5,
+    //     end: innerHeight * 4.5,
+    //     onUpdate: (event) => {
+    //       this.group.rotation.y = event.progress * -0.7;
+
+    //       this.group.position.y = event.progress * -1;
+    //       this.group.position.z = event.progress * -2 + 6;
+    //       this.group.position.x = event.progress * -(xLength - this.boxSize.x);
+    //     },
+    //     onLeave: (e) => {
+    //       console.log('leave');
+    //     },
+    //     onEnterBack: () => {
+    //       console.log('onEnterBack');
+    //     },
+    //   },
+    // });
+  }
+
+  loadPeopleModel() {
+    ThreeHelper.instance.loadGltf('/models/people.glb').then((gltf) => {
+      this.setEnv(gltf.scene);
+      console.log(gltf);
+      this.peopleModel.add(gltf.scene);
+      this.peopleModel.position.z = -0.4;
+
+      const mixer = new AnimationMixer(gltf.scene);
+      const wave = gltf.animations.find((action) => action.name == 'wave') as THREE.AnimationClip;
+      mixer.clipAction(wave).play();
+
+      this.animation = (p: number) => {
+        const time = p * wave.duration;
+        mixer.setTime(time);
+      };
     });
   }
 }
